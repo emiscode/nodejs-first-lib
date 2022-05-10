e2004572e92207409ed300bdbfd439fa0a9a6e05
Emilio <emiscode@gmail.com>
2022-05-10 15:15:55 -0300

    Read file sync, async and with promise

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+console.log(chalk.red('start'));
+
 const handleError = function(err) {
     throw new Error(chalk.red(err));
 }
 
-const getFile = function(filePath) {
+const getFileSync = function(filePath) {
+    try {
+        const encoding = 'utf-8';
+        const file = fs.readFileSync(filePath, encoding);
+        console.log(file);
+    } catch(err) {
+        handleError(err);
+    }
+}
+
+const getFileAsync = function(filePath) {
     const encoding = 'utf-8';
 
     fs.readFile(filePath, encoding, function(err, data) {
@@ -16,4 +28,23 @@ const getFile = function(filePath) {
     });
 }
 
-getFile('./files/file-01.m');
+const getFilePromise = function(filePath) {
+    const encoding = 'utf-8';
+
+    fs.promises.readFile(filePath, encoding)
+    .then(data => {
+        console.log(chalk.green(data));
+    })
+    .catch(err => {
+        handleError(err)
+    })
+}
+
+const pathToFile = './files/file-01.md';
+const pathToFileWrong = pathToFile + 'wrong';
+
+//getFileSync(pathToFile);
+//getFileAsync(pathToFile);
+getFilePromise(pathToFile);
+
+console.log(chalk.red('end'));
